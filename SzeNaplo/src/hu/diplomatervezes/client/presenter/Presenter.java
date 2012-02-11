71991165ed72ba67a165120e45712bb109849b33
kojszagergo <kojszagergo@gmail.com>
2012-02-11 10:24:13 +0000

    MVC-be átrakva

---
+++ SzeNaplo/src/hu/diplomatervezes/client/presenter/Presenter.java
@@ -0,0 +1,9 @@
+package hu.diplomatervezes.client.presenter;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public abstract interface Presenter {
+	
+	public abstract void go(final HasWidgets container);
+	
+}
