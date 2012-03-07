d57f3c1cac6a36bda442d4a8cbc3a8d74196efd9
kojszagergo <kojszagergo@gmail.com>
2012-03-07 10:03:14 +0000

    kezdeti

---
+++ SzeNaplo/src/hu/diplomatervezes/client/LoginInfo.java
@@ -0,0 +1,57 @@
+package hu.diplomatervezes.client;
+
+import java.io.Serializable;
+
+public class LoginInfo implements Serializable {
+
+	private boolean loggedIn = false;
+	private String loginUrl;
+	private String logoutUrl;
+	private String emailAddress;
+	private String nickName;
+	
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+	
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+	
+	public String getLoginUrl() {
+		return loginUrl;
+	}
+	
+	public void setLoginUrl(String loginUrl) {
+		this.loginUrl = loginUrl;
+	}
+
+	public String getLogoutUrl() {
+		return logoutUrl;
+	}
+
+	public void setLogoutUrl(String logoutUrl) {
+		this.logoutUrl = logoutUrl;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	
+	
+	
+	
+}
