e3ca75e8d21659bbd9bb99fde54c3005b474c4ea
lenardzs@gmail.com <lenardzs@gmail.com@d58e8639-c954-ce80-675f-b1cb3dbaaa4c>
2012-05-11 12:58:10 +0000

    Tanar es a Tantagy osztalyok feltoltese

--- trunk/SzeNaplo/src/hu/diplomatervezes/shared/Tantargy.java
+++ trunk/SzeNaplo/src/hu/diplomatervezes/shared/Tantargy.java
@@ -4,13 +4,14 @@
 package hu.diplomatervezes.shared;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Date;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
+
 
 /**
  * @author Kojsza Gergely
@@ -22,37 +23,58 @@ public class Tantargy implements Serializable {
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	private Long id;
+	
 	@Persistent
 	private String tantargyNeve;
+	
 	@Persistent
 	private String rovidNev;
+//	letrehozasa datuma
+	@Persistent
+	private Date dateCreated;
+//	objketum verzio tarolasa
+	@Persistent
+	private Long version;
 	
 	public Tantargy() {}
 	
 	public Tantargy(String tantargyNeve, String rovidNev) {
 		this.tantargyNeve = tantargyNeve;
 		this.rovidNev = rovidNev;
+		setDateCreated();
+		incrementVersion();
 	}
 
-	public String getTantargyNeve() {
-		return tantargyNeve;
-	}
+	public String getTantargyNeve() {return tantargyNeve;}
+	public void setTantargyNeve(String tantargyNeve) {this.tantargyNeve = tantargyNeve;}
 
-	public void setTantargyNeve(String tantargyNeve) {
-		this.tantargyNeve = tantargyNeve;
-	}
+	public String getRovidNev() {return rovidNev;}
+	public void setRovidNev(String rovidNev) {this.rovidNev = rovidNev;}
 
-	public String getRovidNev() {
-		return rovidNev;
-	}
-
-	public void setRovidNev(String rovidNev) {
-		this.rovidNev = rovidNev;
-	}
-
-	public Long getId() {
-		return id;
+	public Long getId() {return id;}
+//  itt eseteleg meghivhato az incrementVersion metodus	
+	public void setVersion(Long version){this.version = version;}
+//  objekum verzio beallitasa
+	public Long getVersion(){
+		if (version == null) {
+			version = (long) 01;
+		}
+		return version;
 	}
 	
+//	verzio novelese	
+	private void incrementVersion() {
+		if (version == null){
+			version = (long) 01;
+		} else {version++;}
+	}
+//	letrehozas datum beallitasa
+	public void setDateCreated() {
+		if (dateCreated == null) {
+			dateCreated = new Date();
+		}
+	}
+	
+	public Date getDateCreated(){return dateCreated;}
 	
 }
