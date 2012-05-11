cccfb9355c4a50ea60f6dee6650520ea878eb789
lenardzs <lenardzs@gmail.com>
2012-05-11 13:33:58 +0000

    osztaly bovitese a addNewsubject metodussal deleteSubject modifySubject metoduok megvalositasa meg nincs kesz

--- SzeNaplo/src/hu/diplomatervezes/server/StudentServiceImpl.java
+++ SzeNaplo/src/hu/diplomatervezes/server/StudentServiceImpl.java
@@ -6,9 +6,11 @@ import javax.jdo.PersistenceManagerFactory;
 
 import hu.diplomatervezes.client.StudentService;
 import hu.diplomatervezes.shared.Diak;
+import hu.diplomatervezes.shared.Tantargy;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+@SuppressWarnings("serial")
 public class StudentServiceImpl extends RemoteServiceServlet implements
 		StudentService {
 	
@@ -19,8 +21,35 @@ public class StudentServiceImpl extends RemoteServiceServlet implements
 		// TODO Auto-generated method stub
 
 	}
+	
+	@Override
+	//ellenorozni kell uj tantargy eseten, hogy letezik-e mar az adatbazisban
+	public void addNewSubject(Tantargy ujTantargy) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		
+		try {
+			pm.makePersistent(ujTantargy);
+		} 
+		finally {
+			pm.close();
+		}		
+	}
+	
+	@Override
+	public void deleteSubject(Tantargy regiTantargy) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
+	public void modifySubject(Tantargy regiTantargy) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	//ellenorozni kell uj diak felvitele eseteben, hogy letezik-e mar az adatbazisban
 	public void addNewStudent(Diak newStudent) {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		
@@ -31,6 +60,8 @@ public class StudentServiceImpl extends RemoteServiceServlet implements
 			pm.close();
 		}		
 	}
+
+
 	
 
 }
